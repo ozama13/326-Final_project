497e763248580502e84cc2afdc9fb94c947aa62c
JaclynW <Jaclynwelfeld@yahoo.com>
2022-04-13 19:00:41 -0400

    building up

--- musicshare.py
+++ musicshare.py
@@ -1,2 +1,32 @@
 #326 Final Project
 #BY Larissa Warthen, Jaclyn Welfeld, Olivia Zama
+
+
+
+class User:
+
+class Song:
+
+    def __init__(self title, artist, genre):
+    self.title = title
+    self.artist = artist
+    self.genre = genre
+
+
+
+
+
+
+
+
+def main ():
+    songReccs = []
+
+    return songReccs
+
+if __name__ == "__main__":
+
+
+    
+
+
