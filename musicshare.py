cb55dad991f0de539e59ea870ab6a6c42a4312c0
JaclynW <Jaclynwelfeld@yahoo.com>
2022-04-17 22:12:12 -0400

    Update musicshare.py

--- musicshare.py
+++ musicshare.py
@@ -2,17 +2,32 @@
 #BY Larissa Warthen, Jaclyn Welfeld, Olivia Zama
 
 
-
 class User:
+    """Class for the users getting music reccomendations
+
+    Attributes:
+        _type_: _description_
+    """
 
 class Song:
+    """Class for song objects
 
-    def __init__(self title, artist, genre):
-    self.title = title
-    self.artist = artist
-    self.genre = genre
+    Attributes: 
+        title ([string]): [x coordinate of the car]
+        artist ([string]): [y coordinate of the car]
+        genre ([string]): [starting heading of the car]
+    """
+    def __init__(self, title, artist, genre):
+        """initializes song qualities including title, artist, and genre
 
-
+        Args:
+        title (string): title of song
+        artist (string): artist of song
+        genre (string): genre of song
+        """
+        self.title = title
+        self.artist = artist
+        self.genre = genre
 
 
 
@@ -20,6 +35,17 @@ class Song:
 
 
 def main ():
+    """Creates song reccomendations for the user 
+
+    Args:
+
+    
+    Returns:
+        list: list of reccomended songs for the user
+
+    Side Effects:
+    
+    """
     songReccs = []
 
     return songReccs
