8b435ae22717a6be5074704ac237c460477414b6
JaclynW <Jaclynwelfeld@yahoo.com>
2022-04-27 19:14:58 -0400

    current all docstrings

--- musicshare.py
+++ musicshare.py
@@ -10,14 +10,25 @@ class User:
     """
     
     def __init__(self, username, password, email):
+        """initializes user attributes
+
+        Args:
+        username (string): user's spotify username
+        password (string): user's spotify password
+        email (string): user's email connected to spotify 
+        """
         self.username = username
         self.password = password
         self.email = email
         
     def playlist(self):
+        """list of User's personal songs aka the playlist of music they already like and listen to
+        """
         pass
     
     def recomendList(self):
+        """list of new songs for the user that are reccomended due to their friend's playlist
+        """
         pass
 
 class Song:
@@ -62,6 +73,8 @@ def main ():
     return songReccs
 
 if __name__ == "__main__":
+    pass
+
 
 
     
