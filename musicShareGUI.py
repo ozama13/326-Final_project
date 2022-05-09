fbc6caa0a4261818d36bc3a3d34ef8108e25fe66
ozama13 <102765937+ozama13@users.noreply.github.com>
2022-05-09 11:48:31 -0400

    gui creation
    -oz

--- musicShareGUI.py
+++ musicShareGUI.py
@@ -1,2 +1,60 @@
 #test 
+#import module
+from tkinter import *
 
+#creating root window
+root = Tk()
+
+#root window title and dimension
+root.title("Welcome to Music Share!")
+#Set geometry (widthxheight)
+root.geometry('750x250')
+
+#setting up Tkinter frame
+def display_text():
+    username = user_name.get()
+    Label(ws, text=f'Welcome,{username},!',pady=20).pack()
+
+ #enter button to move to next window
+ user_name= Entry(ws)
+ username.pack(pady=30)
+
+#creating next window for playlist entry
+
+Button(
+    ws,
+    text = "Enter"
+    padx=10
+    pady=5
+    command=display_text
+).pack()
+#next window
+ws.mainloop()
+
+root.configure(background='grey')
+
+#simple form to create playlist using user input
+
+playlist_entry = Label(root, text="Input your selected songs:")
+genre_menu = StringVar(root)
+genre= OptionMenu(root, genre_menu,
+    'Rap',
+    'Country',
+    'Pop',
+    'EDM',
+    'Rock',
+    'Soul',
+    'Funk').grid(row=0, column=0)
+genre.pack()
+artist = Label(root, text='Artist Name').grid(row=1, column=0)
+song = Label(root, text='Song Title').grid(row=2, column=0)
+a1= Entry(root).grif(row=0,column=1)
+a2= Entry(root).grif(row=1,column=1)
+a3= Entry(root).grif(row=2,column=1)
+
+#after user entry
+
+def clicked():
+    res='Hold on, we are generating a shared playlist'+txt.get()
+
+btn= Button(root, text="Submit").grid(row=4, column=0)
