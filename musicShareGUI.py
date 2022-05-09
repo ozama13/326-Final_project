d531daa47f2678a3447d0f8ff38dffaa079e3be1
ozama13 <102765937+ozama13@users.noreply.github.com>
2022-05-09 18:31:19 -0400

    gui for log in
    oz

--- musicShareGUI.py
+++ musicShareGUI.py
@@ -5,36 +5,76 @@ import musicshare
 import song
 import user
 
+
+def main_account_screen();
+
 #creating root window
-root = Tk()
-
+main_account.root = Tk()   
 #root window title and dimension
-root.title("Welcome to Music Share!")
+main_account.root.title("Welcome to Music Share!")
 #Set geometry (widthxheight)
-root.geometry('750x250')
+main_account.root.geometry('750x250')
 
-#setting up Tkinter frame
-def display_text():
-    username = user_name.get()
-    Label(ws, text=f'Welcome,{username},!',pady=20).pack()
+#setting up labels for user input
+Label(text="Login or Signup", bg='papayawhip',width=300, height='2',font=('Arial',14)).pack()
+Label(text='').pack()
 
- #enter button to move to next window
- user_name= Entry(ws)
- username.pack(pady=30)
+#creating login button
+Button(text="Login",height='2', width='30').pack()
+#creating signup button
+Button(text='Register', height='2',width='30').pack()
 
-#creating next window for playlist entry
+#start GUI
+main_screen.mainloop()
+#call main_account_screen
+main_account_screen()
 
-Button(
-    ws,
-    text = "Enter"
-    padx=10
-    pady=5
-    command=display_text
-).pack()
-#next window
-ws.mainloop()
+#signup window:
+def signup():
+    signup_screen= Toplevel(main_screen)
+    signup_screen.title('Signup')
+    signup_screen.geometry('300x250')
+#text variables
+username=StringVar()
+password=StringVar()
+#labels for signup instructions
+Label(signup_screen, text="Please enter your information below", bg='papayawhip').pack()
+Label(signup_screen, text='').pack()
+username_lable = Label(register_screen, text="Username * ")
+    username_lable.pack()
+ 
+# Set username entry
+# The Entry widget is a standard Tkinter widget used to enter or display a single line of text.
+    
+    username_entry = Entry(signup_screen, textvariable=username)
+    username_entry.pack()
+   
+# Set password label
+    password_lable = Label(signup_screen, text="Password * ")
+    password_lable.pack()
+    
+# Set password entry
+    password_entry = Entry(signup_screen, textvariable=password, show='*')
+    password_entry.pack()
 
-root.configure(background='grey')
+#set email label
+    email_lable = Label(signup_screen, text="Email * ")
+    email_lable.pack()
+
+#set email entry
+    email_entry = Entry(signup_screen, textvariable=email, show='*')
+    email_entry.pack()
+
+
+    Label(signup_screen, text="").pack()
+    
+# Set signup button
+    Button(signup_screen, text="Signup", width=10, height=1, bg="papayawhip").pack()
+
+global main_screen
+
+#signup button
+Button(text='Signup', height='2',width='30',command=Register).pack()
 
 #simple form to create playlist using user input
 
