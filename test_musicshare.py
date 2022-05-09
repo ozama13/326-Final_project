5c6b6d8083dcec94d2d7f7943459b606a1dc99fb
JaclynW <Jaclynwelfeld@yahoo.com>
2022-05-09 16:35:33 -0400

    Update test_musicshare.py
    more tests <3

--- test_musicshare.py
+++ test_musicshare.py
@@ -1,3 +1,4 @@
+import unittest
 import musicshare
 import song
 import user
@@ -7,5 +8,17 @@ def test_song():
     assert song1.title == '505','wrong title'
     assert song1.artist == 'Arctic Monkeys','wrong artist'
     assert song1.genre == 'Rock','wrong genre'
-    
-    
+
+def test_user():
+    user1 = user.User(username= 'ssmith', password= 'ssmith12', email= 'ssmith@rocketmail.com')
+    assert user1.username == 'ssmith','wrong username login'
+    assert user1.artist == 'ssmith12','wrong password'
+    assert user1.genre == 'ssmith@rocketmail.com','wrong email'
+
+def test_addFriend():
+    user1 = "Ilana"
+    user2 = "Esther"
+    user1.AddFriend(user2)
+    assertTrue(user2 is in user1.friends, 'we aint chill like that') 
+
+
