e6bf3a4774aaea2924b26a636bc0b78be7e2f4ff
llwar <102543156+llwar@users.noreply.github.com>
2022-05-09 16:38:12 -0400

    minor changes

--- test_musicshare.py
+++ test_musicshare.py
@@ -2,10 +2,11 @@ import musicshare
 import song
 import user
 
-def test_song():
-    song1 =  song.Song(title= '505', artist= 'Arctic Monkeys', genre= 'Rock')
+def test_song_build():
+    song1 =  song.Song(title= "505", artist= "Arctic Monkeys", genre= "Rock")
     assert song1.title == '505','wrong title'
     assert song1.artist == 'Arctic Monkeys','wrong artist'
     assert song1.genre == 'Rock','wrong genre'
+    
     
     
